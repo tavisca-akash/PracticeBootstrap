bc6de5d123f2b6b2f285f00f8b3512b8738121f0
unknown <abhandwalkar@TRAINING3.internal.tavisca.com>
2015-08-19 10:27:34 +0530

    cricket updated

---
+++ Cricket/pages/SignUp.aspx.cs
@@ -0,0 +1,30 @@
+﻿using HelperClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bootstrap.pages
+{
+    public partial class SignUp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void SignUp_Clicked(object sender, EventArgs e)
+        {
+            User user = new User();
+            user.age = int.Parse(Age.Value);
+            user.firstName = FirstName.Value;
+            user.lastName = LastName.Value;
+            user.password = Password.Value;
+            user.email = Email.Value;
+            user.player = size.Value;
+            AddUser.SaveUser(user);
+        }
+    }
+}
